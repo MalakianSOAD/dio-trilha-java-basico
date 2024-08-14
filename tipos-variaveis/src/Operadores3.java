6024e35984e27a26e85977125f0a7cfe567ed1f9
tiago <moreiratiagopaulo@gmail.com>
2024-08-14 10:57:41 -0300

    exemplo de condiçoes com boolean

---
+++ tipos-variaveis/src/Operadores3.java
@@ -0,0 +1,14 @@
+public class Operadores3 {
+    public static void main(String[] args) {
+        boolean condiçao1 = true;
+        boolean condiçao2 = false;
+        if (condiçao1 && condiçao2) {
+            System.out.println("as duas condições sao verdaeiras0");
+
+
+            
+        }
+        else
+            System.out.println("alguma das condiçoes é falsa");
+    }
+}
