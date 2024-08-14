a8426d66963e744b6d76aa7427dbd5a4ae5b485c
tiago <moreiratiagopaulo@gmail.com>
2024-08-14 10:43:54 -0300

    adição de exemplo de (.equals)

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,9 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+      String nomeUm = "tiago";
+      String nomeDois = new String("tiago");
+      
+      System.out.println(nomeUm.equals(nomeDois));
+    }
+    
+}
